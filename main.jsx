77e224bbef1fef51df2f16cd51e1f3ba388b9508
oatmealdoc <60593962+oatmealdoc@users.noreply.github.com>
2025-06-03 22:28:51 -0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TruCoreCalculator from './components/TruCoreCalculator';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TruCoreCalculator />
+  </React.StrictMode>
+);
